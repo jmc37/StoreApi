920c807d286801704befdae82366b68b4638646d
Joseph Chan <josephchan36@gmail.com>
2023-08-15 12:13:09 -0700

    Added database migration with Alembic and Flask-Migrate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from db import db
 from blocklist import BLOCKLIST
 import models
@@ -25,6 +26,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "jose"
@@ -86,9 +88,6 @@ def create_app(db_url=None):
             ),
             401,
         )
-
-    with app.app_context():
-        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
