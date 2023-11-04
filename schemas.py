cf910b4c5cae23b5490ae7a7b660179c7a4308c8
Joseph Chan <josephchan36@gmail.com>
2023-11-03 20:55:51 -0700

    updated schema to include description

--- schemas.py
+++ schemas.py
@@ -5,6 +5,7 @@ class PlainItemSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
+    description = fields.Str(required=True)
     store_id = fields.Str(required=True)
     image = fields.Str(required=True)
 
